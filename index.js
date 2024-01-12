c9785b03bbbe3a5c09771f7a3e5ff5d94193cd0d
serghei madan <madan.serghei@yahoo.com>
2024-01-12 14:47:01 -0800

    fixing app

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 import app from "./server.js"
 import connectDB from "./config/db.config.js";
 
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import allRoutes from "./routes/index.js";
+import { corsOptions } from "./config/cors/corsOptions.js";
+import { errorHandlerMiddleware } from "./middleware/error/errorHandlerMiddleware.js";
+
+//start server
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// to serve files from uploads directory
+// app.use("/static", express.static("static"));
+
+// app.use("/api", allRoutes);
+app.use('/', (req, res) => {
+    res.send('Welcome to ESI Server')
+})
+
+// Error Middleware
+app.use(errorHandlerMiddleware);
+
+export default app
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (err) => {
   if (err) return console.log(err);
-  connectDB();
+//   connectDB();
   console.log(`Server running on port: ${PORT}`);
 });  
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import allRoutes from "./routes/index.js";
-import { corsOptions } from "./config/cors/corsOptions.js";
-import { errorHandlerMiddleware } from "./middleware/error/errorHandlerMiddleware.js";
+// import express from "express";
+// import morgan from "morgan";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import bodyParser from "body-parser";
+// import allRoutes from "./routes/index.js";
+// import { corsOptions } from "./config/cors/corsOptions.js";
+// import { errorHandlerMiddleware } from "./middleware/error/errorHandlerMiddleware.js";
 
-//start server
-const app = express();
+// //start server
+// const app = express();
 
-app.use(morgan("tiny"));
-app.use(express.json());
-app.use(cors(corsOptions));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(morgan("tiny"));
+// app.use(express.json());
+// app.use(cors(corsOptions));
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
 
-// to serve files from uploads directory
-app.use("/static", express.static("static"));
+// // to serve files from uploads directory
+// app.use("/static", express.static("static"));
 
-app.use("/api", allRoutes);
+// app.use("/api", allRoutes);
 
-// Error Middleware
-app.use(errorHandlerMiddleware);
+// // Error Middleware
+// app.use(errorHandlerMiddleware);
 
-export default app
+// export default app
