23852f79866f3111e37ef9eb9dffe118d9360c79
serghei madan <madan.serghei@yahoo.com>
2024-01-12 14:31:44 -0800

    Index.js | /accounnt route add

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+app.use('/account', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Account Page!')
+})
+app.use('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Home Page!')
 })
 app.listen(process.env.PORT || 3000)
